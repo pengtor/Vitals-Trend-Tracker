fa3952d599364c821a7b5140bb7a710ff7be7b77
Nasria <alfalahynasria@gmail.com>
2026-09-06 21:56:33 -0400

    lonic added

---
+++ src/fhir.js
@@ -0,0 +1,15 @@
+// LOINC codes
+export const LOINC_CODES = {
+  A1C: '4548-4',
+  GLUCOSE: '2339-0',
+};
+
+export async function fetchLabObservations(client, patientId) {
+  const codes = `${LOINC_CODES.A1C},${LOINC_CODES.GLUCOSE}`;
+
+  const bundle = await client.request(
+    `Observation?patient=${patientId}&code=${codes}&_sort=-date`
+  );
+
+  return bundle;
+}
